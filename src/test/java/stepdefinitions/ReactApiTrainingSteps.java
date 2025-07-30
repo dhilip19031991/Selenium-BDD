20b221ad6e3bdab1158312c26212a35432668d6f
dhilip19031991 <dhilip.jaishankar17@gmail.com>
2025-07-30 16:00:36 +0530

    ReactApi-GetMethod and restassured pomxml

---
+++ src/test/java/stepdefinitions/ReactApiTrainingSteps.java
@@ -0,0 +1,37 @@
+package stepdefinitions;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+import pages.ReactApiTraining;
+
+public class ReactApiTrainingSteps {
+ReactApiTraining reactapitraining= new ReactApiTraining();
+Response response;
+@Given("url and getting all training records")
+public void url_and_getting_all_training_records() {
+    response = reactapitraining.allTrainingRecords();
+}
+@Then("verify response code should be {int}")
+public void verify_response_code_should_be(int expectedstatuscode) {
+  Assert.assertEquals(expectedstatuscode, response.getStatusCode());
+}
+@Then("verify response has list of trainings")
+public void verify_response_has_list_of_trainings() {
+  Assert.assertFalse(response.jsonPath().getList("$").isEmpty());
+}
+@Then("print first training record")
+public void print_first_training_record() {
+List<Map<String, Object>> trainings = response.jsonPath().getList("$");
+    if (!trainings.isEmpty()) {
+        System.out.println("First training record: " + trainings.get(0));
+    } else {
+        System.out.println("No training records found");
+    }
+}
+}
