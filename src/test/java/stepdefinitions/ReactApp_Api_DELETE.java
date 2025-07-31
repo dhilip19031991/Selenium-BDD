c53ec7dee600db410a030ff407d4650025defb4b
mary-margaret-amm <maggy.amm@gmail.com>
2025-07-31 23:50:58 +0530

    api_latest

--- src/test/java/stepdefinitions/ReactApp_Api_DELETE.java
+++ src/test/java/stepdefinitions/ReactApp_Api_DELETE.java
@@ -2,16 +2,35 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static io.restassured.RestAssured.given;
 
 public class ReactApp_Api_DELETE {
+    private String response4;
 
     @When("I send a DELETE request to the API with endpoint {string}")
     public void i_send_a_delete_request_to_the_api_with_endpoint(String endpointC) {
+        String response4 = given().log().all().header("Content-Type", "application/json")
+                .when().delete(endpointC).then().log().all().assertThat().statusCode(204)
+                .extract().asString();
+
+        this.response4 = response4;
 
     }
 
-    @Then("verify the status code has {string}")
-    public void verify_the_status_code_has(String string) {
+    @Then("I should receive the delete response")
+    public void i_should_receive_the_delete_response() {
+        System.out.println(response4);
+
+    }
+
+    @Then("verify if the status code has {string}")
+    public void verify_if_the_status_code_has(String codeDel) {
+        boolean status4 = response4.contains(codeDel);
+        if (status4) {
+            System.out.println("Deleted Successfully : " + codeDel);
+        } else {
+            System.out.println("Received the error code");
+        }
 
     }
 
--- src/test/resources/features/ReactApi_GET_POST.feature
+++ src/test/resources/features/ReactApi_GET_POST.feature
@@ -15,7 +15,7 @@ Scenario: Verify POST /api/trainings returns 201 status
 
 Given The base URI of API is "http://10.192.190.130:5000"
 When I send a POST request to the API with endpoint "/api/trainings"
-Then I should receive the response 
+Then I should receive the post response 
 And verify the status code is "HTTP/1.1 201 Created"
 
 
