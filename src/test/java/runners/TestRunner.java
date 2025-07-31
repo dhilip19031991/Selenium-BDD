bc5bf2f22fe01a8658057a271ac75e99e57f0e2c
mary-margaret-amm <maggy.amm@gmail.com>
2025-08-01 01:07:57 +0530

    API_1

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,17 +7,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions", "hooks", "common",
-                "pages" }, plugin = {
-                                "pretty", "html:target/cucumber-reports.html",
-                                "junit:target/junit-results.xml",
-<<<<<<< HEAD
-                                "json:target/cucumber.json" }, monochrome = true,
-                // tags = "@smk",
-                dryRun = false)
-=======
-                                "json:target/cucumber.json" }, monochrome = true, 
-                 tags = "@smoke_api", 
-                 dryRun = false)
->>>>>>> fc51564e618f77cbb311700e5b5ebcac556bdb36
+        "pages" }, plugin = {
+                "pretty", "html:target/cucumber-reports.html",
+                "junit:target/junit-results.xml",
+
+                "json:target/cucumber.json" }, monochrome = true, tags = "@smoke_API", dryRun = false)
+
 public class TestRunner {
 }
