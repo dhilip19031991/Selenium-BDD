220566f3cdeffaec8c224ccc6080cf427c5149ef
SanthoshSanto18 <66413587+SanthoshSanto18@users.noreply.github.com>
2025-10-27 12:03:38 +0530

    added tag

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,7 +13,7 @@ import io.cucumber.junit.CucumberOptions;
                 "junit:target/junit-results.xml",
                 "json:target/cucumber.json"
         }, monochrome = true,
-        // tags = "@smoke_API",
+         tags = "@smoke_API",
         dryRun = false)
 public class TestRunner {
 }
